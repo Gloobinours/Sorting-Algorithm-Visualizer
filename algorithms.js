b8c1d3bc70f66dc19b94db9a0926b2cfe0e78076
Louan Flammanc <kusami.louan@gmail.com>
2022-08-01 09:56:46 -0400

    bubble_sort() fix

--- algorithms.js
+++ algorithms.js
@@ -8,13 +8,13 @@ an_array = [5,1,9,7,8,3,6,2];
  * @returns sorted array
  */
 const bubble_sort = (an_array) => {
-    for (let i = 0; i < an_array.length; i++) {
-        if (an_array[i] > an_array[i+1]) {
-            let a = an_array[i];
-            let b = an_array[i + 1];
-            temp = a;
-            a = b;
-            b = temp;
+    for (let i = 0; i < an_array.length -1; i++) {
+        for (let j = 0; j < an_array.length -1 - i; j++) {
+            if (an_array[j] > an_array[j+1]) {
+                temp = an_array[j];
+                an_array[j] = an_array[j+1];
+                an_array[j+1] = temp;
+            }
         }
     }
 
