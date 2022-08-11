8d5c0b9498e1e790482c8b8757bdd9b39c800c7e
Samuel Coniglio <samuelconiglio2003@gmail.com>
2022-08-11 16:58:07 -0400

    got bubble sort to work, basically

--- algorithms.js
+++ algorithms.js
@@ -10,45 +10,46 @@ an_array = [5,1,9,7,8,3,6,2];
  */
 const bubble_sort = (an_array, blockSwapCallback) => {
     console.log("test1");
-    make_bars(document.getElementById("slider").value, an_array);
+    let timeoutDuration = 1;
+    make_bars(parseInt(document.getElementById("slider").value), an_array);
     for (let i = 0; i < an_array.length -1; i++) {
         for (let j = 0; j < an_array.length -1 - i; j++) {
             if (an_array[j] > an_array[j+1]) {
                 temp = an_array[j];
                 an_array[j] = an_array[j+1];
                 an_array[j+1] = temp;
-                blockSwapCallback(j, j+1);
             }
+            setTimeout(blockSwapCallback, timeoutDuration * 1000, j, j+1);
+            timeoutDuration++;
         }
     }
 }
 
 const make_bars = (slider_value, an_array) => {
     let referenceToContainer = document.getElementById("screen");
-    let max_value = Math.max(an_array);
+    let max_value = Math.max(...an_array);
     for(let i = 0 ; i < slider_value; i++) {
         let child = document.createElement("div");
-        if(i == 0) {
-            console.log(child);
-        }
         child.style.height = ((an_array[i]/max_value) * 100).toString() + "%";
         child.style.width = (100/an_array.length).toString() + "%";
-        child.style.border = "solid 1px black";
+        child.style["background-color"] = "rgb(0, 0, 0)";
         child.style.display = "inline-block";
         referenceToContainer.appendChild(child);
     }
 }
 
-const blockSwapCallback = (i, j) => {
-    let referenceToContainer = document.getElementById("screen");
-    //console.log(referenceToContainer);
-    let bar1 = referenceToContainer.childNodes.item(i);
-    let bar2 = referenceToContainer.childNodes.item(j);
-    let height1 = bar1.style.height;
-    let height2 = bar1.style.height;
-    bar1.style.height = height2;
-    bar2.style.height = height1;
-    setTimeout(function(){}, 1000);
+const timeout_debug = () => {
+    console.log("timeout ended");
 }
 
+
+const blockSwapCallback = (i, j) => {
+    let referenceToContainer = document.getElementById("screen");
+    let bar1 = referenceToContainer.children[i];
+    let bar2 = referenceToContainer.children[j];
+    let height1 = bar1.style.height;
+    let height2 = bar2.style.height;
+    bar1.style.height = height2;
+    bar2.style.height = height1;
+}
 console.log(bubble_sort(an_array, blockSwapCallback));
