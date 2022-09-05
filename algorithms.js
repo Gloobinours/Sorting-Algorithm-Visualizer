3653c26e6db8e673f0dca3d81aeb4c3a0d25e5c9
Samuel Coniglio <samuelconiglio2003@gmail.com>
2022-09-05 18:34:50 -0400

    serious bugs

--- algorithms.js
+++ algorithms.js
@@ -49,19 +49,19 @@ const bubbleSort = (anArray) => {
                 temp = anArray[j];
                 anArray[j] = anArray[j+1];
                 anArray[j+1] = temp;
-                // let animationMatrix = 
-                // [
-                //     [j, j+1], 
-                //     [j, j+1]
-                // ];
-                // sortAnimations.push(animationMatrix);
+                let animationMatrix = 
+                [
+                    [j, j+1], 
+                    [j, j+1]
+                ];
+                sortAnimations.push(animationMatrix);
             } else {
-                // let animationMatrix = 
-                // [
-                //     [j, j +1], 
-                //     [0, 0]
-                // ];
-                // sortAnimations.push(animationMatrix); // add empty animation
+                let animationMatrix = 
+                [
+                    [j, j +1], 
+                    []
+                ];
+                sortAnimations.push(animationMatrix); // add empty animation
             }
         }
     }
@@ -77,91 +77,173 @@ const selectionSort = (anArray) => {
             if (anArray[currentIndex] < anArray[currentMinimumIndex]){
                 currentMinimumIndex = currentIndex;
             }
-            // let animationMatrix = 
-            // [
-            //     [currentIndex, currentMinimumIndex],
-            //     [0, 0]
-            // ];
-            // sortAnimations.push(animationMatrix);
+            let animationMatrix = 
+            [
+                [currentIndex, currentMinimumIndex],
+                []
+            ];
+            sortAnimations.push(animationMatrix);
         }
         let valueOriginallyAtFirstNonPartitionIndex = anArray[firstNonePartitionIndex];
         anArray[firstNonePartitionIndex] = anArray[currentMinimumIndex];
         anArray[currentMinimumIndex] = valueOriginallyAtFirstNonPartitionIndex;
-        // let animationMatrix = 
-        // [
-        //     [firstNonePartitionIndex, currentMinimumIndex], 
-        //     [firstNonePartitionIndex, currentMinimumIndex]
-        // ];
-        // sortAnimations.push(animationMatrix);
+        let animationMatrix = 
+        [
+            [firstNonePartitionIndex, currentMinimumIndex], 
+            [firstNonePartitionIndex, currentMinimumIndex]
+        ];
+        sortAnimations.push(animationMatrix);
     }
     return sortAnimations;
 }
 
-const mergeSort = (anArray) => {
+const mergeSortInPlace = (anArray, startIndex, length, sortAnimations) => {
+    console.log(anArray);
+    console.log(startIndex);
+    console.log(length);
+    console.log("------------------------------------------------------------");
+
     //Return if the array has no elements or just one element.
-    if(anArray.length < 2) return
+    if (length < 2) return
 
-    //Create the two halfs of the array
-    let halfLength = Math.floor(anArray.length / 2);
-    let leftHalf = Array(halfLength).fill(0);
-    let rightHalf = Array(anArray.length - halfLength).fill(0);
+    let halfLength = Math.floor(length / 2);
 
-    // debugger;
+    let leftStartIndex = startIndex;
+    let leftLength = halfLength;
+    let rightStartIndex = startIndex + halfLength;
+    let rightLength = length - halfLength;
 
-    for(let i = 0; i < halfLength; i++){
-        leftHalf[i] = anArray[i];
+    mergeSortInPlace(anArray, leftStartIndex, leftLength, sortAnimations);
+    mergeSortInPlace(anArray, rightStartIndex, rightLength. sortAnimations);
+
+    merge(anArray, leftStartIndex, leftLength, rightStartIndex, rightLength, sortAnimations);
+}
+
+const merge = (anArray, leftStartIndex, leftLength, rightStartIndex, rightLength, sortAnimations) => {
+    let startIndex = leftStartIndex;
+    let length = leftLength + rightLength;
+    resultArray = Array(length).fill(0);
+
+    let iLeft = 0;
+    let iRight = 0;
+    let iResult = 0;
+    while(iLeft < leftLength && iRight < rightLength){
+        let animationMatrix = [
+            [iLeft + leftStartIndex, iRight + rightStartIndex],
+            [],
+            []
+        ];
+        sortAnimations.push(animationMatrix);
+        if(anArray[iLeft + leftStartIndex] <= anArray[iRight + rightStartIndex]){
+            resultArray[iResult] = anArray[iLeft + leftStartIndex];
+            iLeft++;
+            iResult++;
+        } else {
+            resultArray[iResult] = anArray[iRight + rightStartIndex];
+            iRight++;
+            iResult++;
+        }
     }
 
-    for(let i = halfLength; i < anArray.length; i++){
-        rightHalf[i - halfLength] = anArray[i];
-    }
-    // console.log("The array before mergesort has been called: " + anArray);
-    // debugger;
+    while(iLeft < leftLength){
+        let animationMatrix = [
+            [iLeft + leftStartIndex, iLeft + leftStartIndex],
+            [],
+            []
+        ];
+        sortAnimations.push(animationMatrix);
 
-    // console.log("Left half before mergesort has been recursively called: " + leftHalf);
-    // console.log("Right half before mergesort has been recursively called: " + rightHalf);
+        resultArray[iResult] = anArray[iLeft + leftStartIndex];
+        iLeft++;
+        iResult++;
+    }
+
+    while(iRight < rightLength){
+        let animationMatrix = [
+            [iRight + rightStartIndex, iRight + rightStartIndex],
+            [],
+            []
+        ];
+        sortAnimations.push(animationMatrix);
+        resultArray[iResult] = anArray[iRight + rightStartIndex];
+        iRight++;
+        iResult++;
+    }
+
+    for(iResult = 0; iResult < length; iResult++){
+        anArray[leftStartIndex + iResult] = resultArray[iResult];
+        //set
+        let animationMatrix = [
+            [leftStartIndex + iResult, leftStartIndex + iResult],
+            [],
+            [leftStartIndex + iResult, resultArray[iResult]]
+        ];
+        sortAnimations.push(animationMatrix);
+    }
+}
+
+const mergeSort = (anArray) => {
+
+    let sortAnimations = [];
+    mergeSortInPlace(anArray, 0, anArray.length, sortAnimations);
+    console.log(anArray);
+    return sortAnimations;
+
+    // //Return if the array has no elements or just one element.
+    // if(anArray.length < 2) return
+
+    // //Create the two halfs of the array
+    // let halfLength = Math.floor(anArray.length / 2);
+    // let leftHalf = Array(halfLength).fill(0);
+    // let rightHalf = Array(anArray.length - halfLength).fill(0);
+
+    // for(let i = 0; i < halfLength; i++){
+    //     leftHalf[i] = anArray[i];
+    // }
+
+    // for(let i = halfLength; i < anArray.length; i++){
+    //     rightHalf[i - halfLength] = anArray[i];
+    // }
+
+    // debugger;
 
     // //Recursively call mergesort on the two arrays
-    mergeSort(leftHalf);
-    mergeSort(rightHalf);
-    // console.log("The array after mergesort has been called: " + anArray);
-    // console.log("Left half after mergesort has been recursively called: " + leftHalf);
-    // console.log("Right half after mergesort has been recursively called: " + rightHalf);
+    // mergeSort(leftHalf);
+    // mergeSort(rightHalf);
 
-    merge(anArray, leftHalf, rightHalf);
-    // console.log(anArray);
+    // merge(anArray, leftHalf, rightHalf);
 }
 
-const merge = (anArray, leftHalf, rightHalf) => {
-    let i = 0;
-    let j = 0;
-    let k = 0;
+// const merge = (anArray, leftHalf, rightHalf) => {
+//     let i = 0;
+//     let j = 0;
+//     let k = 0;
 
-    // debugger;
+//     // debugger;
 
-    while((i < leftHalf.length) && (j < rightHalf.length)){
-        if (leftHalf[i] <= rightHalf[j]) {
-            anArray[k] = leftHalf[i];
-            i++;
-        } else {
-            anArray[k] = rightHalf[j];
-            j++;
-        }
-        k++;
-    }
+//     while((i < leftHalf.length) && (j < rightHalf.length)){
+//         if (leftHalf[i] <= rightHalf[j]) {
+//             anArray[k] = leftHalf[i];
+//             i++;
+//         } else {
+//             anArray[k] = rightHalf[j];
+//             j++;
+//         }
+//         k++;
+//     }
 
-    while(i < leftHalf.length){
-        anArray[k] = leftHalf[i];
-        i++;
-        k++;
-    }
+//     while(i < leftHalf.length){
+//         anArray[k] = leftHalf[i];
+//         i++;
+//         k++;
+//     }
 
-    while(j < rightHalf.length){
-        anArray[k] = rightHalf[j];
-        j++;
-        k++;
-    }
-}
+//     while(j < rightHalf.length){
+//         anArray[k] = rightHalf[j];
+//         j++;
+//         k++;
+//     }
+// }
 
 const quickSort = (anArray, lowIndex, highIndex) => {
 
@@ -247,20 +329,31 @@ const blockSwapCallback = (referenceToContainer, matrixArrayIndex, matrixArray) 
         bar2.style["background-color"] = "#FFB4B4";
     }
 
+    let currentMatrix = matrixArray[matrixArrayIndex];
+
     //Select
     //Access the currently selected divs
-    let bar1 = referenceToContainer.children[matrixArray[matrixArrayIndex][0][0]];
-    let bar2 = referenceToContainer.children[matrixArray[matrixArrayIndex][0][1]];
+    let bar1 = referenceToContainer.children[currentMatrix[0][0]];
+    let bar2 = referenceToContainer.children[currentMatrix[0][1]];
     bar1.style["background-color"] = "#008000";
     bar2.style["background-color"] = "#008000";
 
     //Swap
-    bar1 = referenceToContainer.children[matrixArray[matrixArrayIndex][1][0]];
-    bar2 = referenceToContainer.children[matrixArray[matrixArrayIndex][1][1]];
-    let height1 = bar1.style.height;
-    let height2 = bar2.style.height;
-    bar1.style.height = height2;
-    bar2.style.height = height1;
+    if(currentMatrix[1].length == 2){
+        bar1 = referenceToContainer.children[currentMatrix[1][0]];
+        bar2 = referenceToContainer.children[currentMatrix[1][1]];
+        let height1 = bar1.style.height;
+        let height2 = bar2.style.height;
+        bar1.style.height = height2;
+        bar2.style.height = height1;
+    }
+
+    //Set
+    if(currentMatrix[2].length == 2){
+        let bar = referenceToContainer.children[currentMatrix[2][0]];
+        let newHeight = currentMatrix[2][1];
+        bar.style.height = newHeight;
+    }
     currentWaitTarget++;
     if(currentWaitTarget == sortAnimations.length){
         document.getElementById("playButton").innerHTML = "Play";
@@ -273,9 +366,9 @@ var isAnimationRunning = false;
 /*
 Sort animations i is a matrix defined as
 [
-    [0, 1] //This will be what is selected/highlighted before its swapped.
-    [0, 1] //These will be swappped (if necessary; if the indexes are the same then there will be no swapping).
-
+    [index1, index2] //This will be what is selected/highlighted before its swapped.
+    [index1, index2] //These will be swappped (if necessary; if the indexes are the same, or if the array is empty, then there will be no swapping).
+    [index, newValue] //These will set the div's height at the given index to have the given value, this array can be empty to indicate do not set anything.
 ]
 
 The selection two-length array will be used for deselection for the subsequent selection.
@@ -307,63 +400,69 @@ const playAnimation = () => {
 
     // console.log("The array that results from merging the aforementioned arrays: " + anArray);
 
-    let arrayBeingSorted = createArray(document.getElementById("slider").value);
-    console.log("Array before it has been sorted: " + arrayBeingSorted);
-    insertionSort(arrayBeingSorted, 0, (arrayBeingSorted.length - 1));
-    console.log("Array after it has been sorted: " + arrayBeingSorted);
+    // let arrayBeingSorted = createArray(document.getElementById("slider").value);
+    // console.log("Array before it has been sorted: " + arrayBeingSorted);
+    // insertionSort(arrayBeingSorted, 0, (arrayBeingSorted.length - 1));
+    // console.log("Array after it has been sorted: " + arrayBeingSorted);
 
 
-    // const button = document. querySelector('button');
-    // button.disabled = true;
-    // if(currentWaitTarget >= sortAnimations.length) {
-    //     //Reset all the globals
-    //     arrayBeingSorted = [];
-    //     isAnimationRunning = false;
-    //     sortAnimations = [];
-    //     currentWaitTarget = 0;
-    //     timeoutIdArray = [];
-    //     sortAnimations = [];
-    //     // colorChangeIdArray = [];
-    //     // colorChangeIdArray2 = [];
-    //     //Manually destroy the old divs
-    //     while(screen.firstChild) {
-    //         screen.removeChild(screen.lastChild);
-    //     }
-    // }
-    // if(arrayBeingSorted.length == 0) {
-    //     arrayBeingSorted = createArray(document.getElementById("slider").value);
-    //     //Need linear iteration regardless (because if we don't recreate bars, we will iterate through the array that is going to be sorted)
-    //     makeBars(arrayBeingSorted);
-    //     dropDownList = document.getElementById("algorithms");
-    //     if(dropDownList.value == "Bubble Sort") {
-    //         sortAnimations = bubbleSort(arrayBeingSorted);
-    //     } else if(dropDownList.value == "Selection Sort") {
-    //         sortAnimations = selectionSort(arrayBeingSorted);
-    //     }
-    //     console.log(sortAnimations);
-    //     timeoutIdArray = Array(sortAnimations.length).fill(0);
-    //     isAnimationRunning = true;
-    // }
-    // if(isAnimationRunning) {
-    //     isAnimationRunning = false;
-    //     document.getElementById("playButton").innerHTML = "Pause";
-    //     let timeoutDuration = 100;
-    //     let currentDuration = timeoutDuration;
-    //     let referenceToContainer = document.getElementById("screen");
-    //     for (let i = currentWaitTarget; i < sortAnimations.length; i++) {
-    //         let id = setTimeout(blockSwapCallback, currentDuration, referenceToContainer, i, sortAnimations);
-    //         timeoutIdArray[i] = id;
-    //         console.log("Called setTimeout: " + currentDuration + "ms, id = " + id);
-    //         currentDuration += timeoutDuration;
-    //     }
-    // } else if (!isAnimationRunning) {
-    //     isAnimationRunning = true;
-    //     document.getElementById("playButton").innerHTML = "Play";
-    //     for (let i = currentWaitTarget; i < sortAnimations.length; i++) {
-    //         console.log("Called clear timeout: " + timeoutIdArray[i]);
-    //         clearTimeout(timeoutIdArray[i]);
-    //     }
-    // }
-    // button.disabled = false;
+    const button = document. querySelector('button');
+    button.disabled = true;
+    if(currentWaitTarget >= sortAnimations.length) {
+        //Reset all the globals
+        arrayBeingSorted = [];
+        isAnimationRunning = false;
+        sortAnimations = [];
+        currentWaitTarget = 0;
+        timeoutIdArray = [];
+        sortAnimations = [];
+        // colorChangeIdArray = [];
+        // colorChangeIdArray2 = [];
+        //Manually destroy the old divs
+        while(screen.firstChild) {
+            screen.removeChild(screen.lastChild);
+        }
+    }
+    if(arrayBeingSorted.length == 0) {
+        arrayBeingSorted = createArray(document.getElementById("slider").value);
+        //Need linear iteration regardless (because if we don't recreate bars, we will iterate through the array that is going to be sorted)
+        makeBars(arrayBeingSorted);
+        dropDownList = document.getElementById("algorithms");
+        if(dropDownList.value == "Bubble Sort") {
+            sortAnimations = bubbleSort(arrayBeingSorted);
+        } else if(dropDownList.value == "Selection Sort") {
+            sortAnimations = selectionSort(arrayBeingSorted);
+        } else if(dropDownList.value == "Merge Sort"){
+            sortAnimations = mergeSort(arrayBeingSorted);
+        } else if(dropDownList.value == "Quick Sort"){
+            sortAnimations = quickSort(arrayBeingSorted);
+        } else if (dropDownList.value == "Insertion Sort"){
+            sortAnimations = insertionSort(arrayBeingSorted, 0, arrayBeingSorted.length - 1);
+        }
+        console.log(sortAnimations);
+        timeoutIdArray = Array(sortAnimations.length).fill(0);
+        isAnimationRunning = true;
+    }
+    if(isAnimationRunning) {
+        isAnimationRunning = false;
+        document.getElementById("playButton").innerHTML = "Pause";
+        let timeoutDuration = 100;
+        let currentDuration = timeoutDuration;
+        let referenceToContainer = document.getElementById("screen");
+        for (let i = currentWaitTarget; i < sortAnimations.length; i++) {
+            let id = setTimeout(blockSwapCallback, currentDuration, referenceToContainer, i, sortAnimations);
+            timeoutIdArray[i] = id;
+            console.log("Called setTimeout: " + currentDuration + "ms, id = " + id);
+            currentDuration += timeoutDuration;
+        }
+    } else if (!isAnimationRunning) {
+        isAnimationRunning = true;
+        document.getElementById("playButton").innerHTML = "Play";
+        for (let i = currentWaitTarget; i < sortAnimations.length; i++) {
+            console.log("Called clear timeout: " + timeoutIdArray[i]);
+            clearTimeout(timeoutIdArray[i]);
+        }
+    }
+    button.disabled = false;
 }
 document.getElementById("playButton").addEventListener("click", playAnimation);
